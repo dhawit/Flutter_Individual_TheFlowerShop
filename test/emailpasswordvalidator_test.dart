522a110fcb39dda9d40935a720c15359bba2ad12
dhawit <94819472+dhawit@users.noreply.github.com>
2023-03-01 03:08:12 +0545

    final

---
+++ test/emailpasswordvalidator_test.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:stylish/screens/profile/login_screen.dart';
+
+// void main() {
+//   testWidgets('Login screen', (tester) async {
+//     await tester.pumpWidget(const MaterialApp(home: LoginScreen()));
+
+//     final textFinder = find.text('Login');
+
+//     expect(textFinder, findsWidgets);
+//   });
+// }
